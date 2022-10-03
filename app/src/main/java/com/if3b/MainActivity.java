8320c523f45f8cb3be538b48a693ee449ddedc28
AzzarRizky <azzar.arza18@gmail.com>
2022-10-03 11:20:38 +0700

    GetTextNama Undone

--- app/src/main/java/com/if3b/MainActivity.java
+++ app/src/main/java/com/if3b/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnPindah;
+    EditText ETnama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Layout A");
+
+        ETnama = findViewById(R.id.ET_nama);
+        
 
         btnPindah = findViewById(R.id.btn_pindah);
 
--- app/src/main/java/com/if3b/SecondActivity.java
+++ app/src/main/java/com/if3b/SecondActivity.java
@@ -12,5 +12,6 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         getSupportActionBar().setTitle("Layout B");
 
+
     }
 }
